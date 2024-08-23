fb0cbd1defc4ccb5d155d036ab0abf7027cea96e
alisteralex <alister.hosamani@hdsofttech.com>
2024-08-23 12:10:10 +0530

    done

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ const fs = require('fs').promises;
 const app = express()
 const path = require('path');
 const port = process.env.PORT || 5000;
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { log } = require('console');
 require("dotenv").config();
+
 
 app.use(cors())
 app.use(express.json())
+
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash",generationConfig: {responseMimeType: "application/json" }});
 
 
 //first this will execute and configure it
@@ -35,35 +42,14 @@ app.post('/upload', upload.single('file'), async (req, res) => {
   const textExtracted = data.text; //extract the text from that array
 
 
-  //Process text to extract the first 500 words
-  const words = textExtracted.split(/\s+/);
-  const first5000Words = words.slice(0, 5000).join(' ');
-  console.log(first5000Words)
-
-
-
   try {
-      const result = await axios.post('https://api.openai.com/v1/chat/completions', {
-          model: "gpt-4",
-          messages: [{
-              role: 'user',
-            //   content: "Provide a summary of the RFP document in JSON format with keys for 'objective', 'description', 'value', 'duedate' and 'questiondate': " + textExtracted
-              content: "Provide a summary of the RFP document stricly in JSON format with and only for the keys of 'title','department','type','event_id','objective', 'description', 'date_of_release','date_of_clarification','date_of_submission','qualification_criteria','contract_term' and 'point_of_contact'. If there is no valid value for the respective key then return the value as not defined for that key. Stricly do not give any other keys in response other than which is provided in the prompt and also do not give nested array json data. Give the information accurate enough in detail without missing out on any important information. If required provide the summaru splitted in points format inside the value of the respected json keys but make sure no important information is missed out since the information you provide should be in accurate from the information provided." + first5000Words
-            
-        }]
-      }, {
-          headers: {
-              'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`  
-          }
-      });
+            const prompt = "Provide a summary of the RFP document stricly in JSON format with and only for the keys of 'title','department','type','event_id','objective', 'description', 'date_of_release','date_of_clarification','date_of_submission','qualification_criteria','contract_term' and 'point_of_contact'. If there is no valid value for the respective key then return the value as not defined for that key. Stricly do not give any other keys in response other than which is provided in the prompt and also do not give nested array json data. Give the information accurate enough in detail without missing out on any important information. If required provide the summaru splitted in points format inside the value of the respected json keys but make sure no important information is missed out since the information you provide should be in accurate from the information provided." + textExtracted ;
 
-      if (result.data.choices && result.data.choices.length > 0) {
-          const parsedResponse = JSON.parse(result.data.choices[0].message.content);
-          // Send the parsed JSON response directly to the frontend
-          res.status(200).json(parsedResponse);
-      } else {
-          res.status(404).send('No data found in API response');
-      }
+            const result = await model.generateContent(prompt);
+            const parsedData = JSON.parse(result.response.text())
+            res.status(200).json(parsedData);
+
+
   } catch (error) {
       console.error('Failed to fetch response:', error);
       res.status(500).send('Error fetching summary from OpenAI');
@@ -75,6 +61,7 @@ app.listen(port,()=>{
     console.log("server is running on port ",port);
     
 })
+
 
 
 
